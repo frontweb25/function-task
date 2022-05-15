b63938e8067ac625d036b3c6b1b0b208925849d7
Zhenya Ruban <ruban.z1@mail.ru>
2022-05-15 23:47:12 +0300

    unit08

---
+++ UNIT 8. PURE FUNCTION, ФУНКЦИИ ВЫСШЕГО ПОРЯДКА/code/index.js
@@ -0,0 +1,98 @@
+// Pure функции
+
+function squad (n) {
+    return n**2;
+}
+
+// console.log(squad(4));
+
+function squad2(n) {
+    document.querySelector('.out-1').textContent = squad(n);
+}
+
+// squad2(5);
+
+let count = 0;
+
+function myCount(){
+    return count++;
+}
+
+// console.log(myCount());
+// console.log(myCount());
+// console.log(myCount());
+
+function showAlert(){
+    alert('Hello');
+}
+
+// showAlert();
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// for (let i = 0; i < 20; i++) {
+//     console.log(randomInt(10,20));
+// }
+
+function year(d){
+    let today = new Date();
+    let year = today.getFullYear()
+    return (year - d);
+}
+
+console.log(year(2005));
+
+function show(d){
+    console.log(d);
+}
+
+// show(4444);
+
+function hello(){
+    console.log('hello');
+}
+hello.hi = 123;
+
+console.log(hello.hi);
+
+function pOdd() {
+    console.log('odd');
+}
+
+function pEven() {
+    console.log('even');
+}
+
+function myNumber(n, odd, even) {
+    if (n % 2 === 0) return even;
+    else return odd;
+}
+
+let z = myNumber(6, pOdd, pEven);
+// console.log(z);
+z();
+
+const w = [
+    { name: "Ivan", "age" : 25},
+    { name: "Serg", "age" : 35},
+    { name: "Olga", "age" : 27},
+];
+
+
+function mySortAge(arr){
+    return arr.filter(item => item.age > 26 ? true : false);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
